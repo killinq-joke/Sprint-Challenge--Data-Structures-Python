48df9e9dda1b66e3c7d6fa6792ba0ea0f2109d8a
zakaria touzri <zakadmaster@gmail.com>
2020-07-17 17:22:38 +0200

    ADDED: ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,26 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
+        self.arr = []
+        self.current = 0
 
     def append(self, item):
-        pass
+        if len(self.arr) >= self.capacity:
+            self.arr[self.current] = item
+            if self.current == len(self.arr) - 1:
+                self.current = 0
+            else:
+                self.current += 1
+        else:
+            self.arr.append(item)
+            
 
     def get(self):
-        pass
+        return self.arr
+
+print([1, 2, 3][1])
+
+r = RingBuffer(5)
+for i in range(50):
+    r.append(i)
+r.get()
